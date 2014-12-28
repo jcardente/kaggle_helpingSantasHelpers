939a9dac964e20f98f39b6fd38165f592d96a07e
jcardente <jcardente@gmail.com>
2014-12-28 16:14:39 -0500

    Major overhaul to get things to go faster

--- src/helpsanta.jl
+++ src/helpsanta.jl
@@ -10,55 +10,33 @@ using ArgParse
 s = ArgParseSettings()
 @add_arg_table s begin
     "--nelves", "-e"
-        help = "Elf prototype multiplier"
+        help = "Elf count"
         arg_type = Int
-        default = 1
+        default = 900
+    "--ntoys", "-t"
+        help = "Toy Count"
+        arg_type = Int
+        default = 10000000
     "toy_file"
         help = "Toy input file"
         required = true
     "soln_file"
         help = "Solution output file"
         required = true
-      "param_file"
-      help = "Elf prototype params file"
-      required = false
 end
 
-parsed_args   = parse_args(s)
-elf_rep_count = parsed_args["nelves"]
-toy_file      = parsed_args["toy_file"]
-soln_file     = parsed_args["soln_file"]
-#params_file   = parsed_args["param_file"]
-
-myToys  = read_toys(toy_file)
-#params  = readcsv(params_file)
-#myElves = create_elves(params, elf_rep_count)
+parsed_args = parse_args(s)
+num_elves   = parsed_args["nelves"]
+num_toys    = parsed_args["ntoys"]
+toy_file    = parsed_args["toy_file"]
+soln_file   = parsed_args["soln_file"]
 
 start = time()
-#solution, num_elves, last_minute, avg_prod = event_loop(myToys, myElves, soln_file)
-solution, num_elves, last_minute, avg_prod = vectorSolve(myToys, elf_rep_count)
+last_minute, avg_prod = vectorSolve(toy_file, soln_file, num_elves, num_toys)
 elapsed_time = time() - start
 
 score = last_minute * log(1.0 + num_elves)
 
 @printf("Runtime= %.2f \tScore= %d \tProd=%.2f\t LastMin=%d\n",
         elapsed_time, score, avg_prod, last_minute)
-        
-wcsv = open(soln_file, "w")
-write(wcsv,"ToyId,ElfId,StartTime,Duration\n");
-for i in 1:size(solution)[2]
-    toy_id          = solution[1,i]
-    elf_id          = solution[2,i]
-    work_start_time = solution[3,i]
-    work_duration   = solution[4,i]
-    
-    tt = hrs._reference_start_time + Dates.Minute(work_start_time)
-    time_string = join(map(string,[Dates.year(tt) Dates.month(tt) Dates.day(tt) Dates.hour(tt) Dates.minute(tt)]), " ")
-    println(wcsv,
-            toy_id,",",
-            elf_id,",",
-            time_string,",",
-            work_duration)
-end
 
-close(wcsv)
--- src/vectorsol.jl
+++ src/vectorsol.jl
@@ -4,12 +4,9 @@ module vectorsol
 using hrs
 using elfs
 using toys
+using Dates
 
-export vectorSolve
-
-function tlt (a,b) 
-    (a[2] <= b[2]) & (a[3] > b[3])
-end
+export vectorSolve, read_fast, adjust_rating
 
 
 function adjust_rating(rating, sanctioned, unsanctioned)
@@ -18,85 +15,101 @@ function adjust_rating(rating, sanctioned, unsanctioned)
             (elfs._rating_decrease ^ (unsanctioned/60.0))))
 end
 
-function best_start_window(work_duration)
-    
-    if (work_duration > hrs._minutes_in_24h)
-        work_duration = work_duration % hrs._minutes_in_24h
-    end
-    
-    # NB - Pick window that ensures rating adjustment
-    #      is one or greater
-    max_unsanctioned = int(floor(0.165 * work_duration))
-    min_sanctioned   = int(min(hrs._hours_per_day * 60, work_duration - max_unsanctioned))
-    best_start       = hrs._day_start
-    best_end         = hrs._day_end - min_sanctioned
 
-(best_start, best_end)
+function read_fast!(toy_file, myToys, optRatios)
+    toysfile = open(toy_file, "r")
+    readline(toysfile)
+    while !eof(toysfile)
+        row = split(strip(readline(toysfile)),",") # id, arrival_minute, duration
+        id       = int(row[1])
+        arrival  = convert_to_minute(row[2])
+        duration = int(row[3])
+        day      = div(arrival, hrs._minutes_in_24h) + 1
+
+        myToys[:,id] = [id; arrival; duration; day]
+        
+        sanctioned, unsanctioned = get_sanctioned_breakdown(hrs._day_start,
+                                                            duration)
+
+        optRatios[id] =  unsanctioned/sanctioned
+    end
+    close(toysfile)
+
+    nothing
 end
 
-function vectorSolve(myToys, num_elves)
+
+function vectorSolve(toy_file, soln_file, num_elves, num_toys)
     
-    # bucket toys into days
-    num_toys = length(myToys)
-    days     = div([t.arrival_minute for t in myToys], (24*60)) .+ 1;
-    max_day  = maximum(days)   
-    toys_day = Array(Any, max_day)
-    for d in 1:max_day
-        toys_day[d] = Tuple[]
-        for tid in findin(days, d)
-            t = myToys[tid]
-            sanctioned, unsanctioned = get_sanctioned_breakdown(hrs._day_start,
-                                                                t.duration)
-            push!(toys_day[d], (t.id, t.arrival_minute, t.duration, unsanctioned/sanctioned))
-        end
-    end
+    # Create data structures to hold toys
+    myToys    = zeros(Int64, 4, num_toys)
+    optRatios = zeros(Float64, num_toys)
+    toy_done  = falses(num_toys)
+    read_fast!(toy_file, myToys, optRatios)
+    days      = myToys[4,:]'
+    max_day   = maximum(days)
+    
+    # Setup elf matrices
+    next_minutes = ones(Int64, num_elves)  .* hrs._day_start
+    ratings      = ones(Float64,num_elves) .* elfs._start_rating
+    assign_count = zeros(Int64, num_elves)
+    assignments  = zeros(Int64, 4, hrs._hours_per_day * 60, num_elves)
 
-    # Setup elf vectors
-    next_minutes = fill(hrs._day_start, num_elves);
-    ratings      = ones(num_elves) .* elfs._start_rating;    
-    work_hours   = zeros(num_elves)
-    assignments  = Array(Any, num_elves)
-    for i in 1:num_elves
-        assignments[i] = Tuple[]
-    end
-
-    # Array to hold solution
-    solution = Array(Int32, 4, length(myToys))
-    soln_idx = 1    
-
-    # Cycle through end of days or until 
+    # Prep output file
+    wcsv = open(soln_file, "w")
+    write(wcsv,"ToyId,ElfId,StartTime,Duration\n");
+    
+    # Cycle through end of days or until
     current_day    = 0
-    av_toys        = Tuple[]
-    jobs           = Any[]
+    day_idx        = 1    
+    min_av_toy     = 1
+    max_av_toy     = 1
     last_minute    = 0
     assigned_count = 0
-    while (((current_day += 1) <= max_day) ||
-           (length(av_toys) > 0))
-        
+    while (!all(toy_done))
+
+        current_day += 1
         day_start_minute = (current_day - 1) * hrs._minutes_in_24h
         
-        # Add new toys and resort
+        println("Day: $(current_day) Done:$(assigned_count) $(sum(toy_done)) of $(length(toy_done)) $(min_av_toy) $(max_av_toy)")
+                
+        # Add new toys and resort.
+        #
+        # NB - this relies on the fact that the toys
+        #      are ordered by time and assiged sequential
+        #      ids. 
         if (current_day <= max_day)
-            append!(av_toys, toys_day[current_day])
+            # NB - possible no toys arrive on a day
+            if ((day_idx < length(days)) &&
+                (days[day_idx] == current_day))
+                while ((day_idx <= length(days)) &&
+                    (days[day_idx] == current_day))
+                    day_idx += 1;
+                end;
+                max_av_toy = max(max_av_toy, myToys[1,day_idx-1])
+            end
         end
-        sort!(av_toys, lt=tlt)           	
-                   
+
         # iterate over toys until all plans full or
         # no more available toys
-        leftover_toys = Tuple[]
-        done_mask     = map(x -> x < (day_start_minute + hrs._day_end) ? 1.0 : Inf, next_minutes)
-        while ((length(av_toys) > 0) &&
-               (any(done_mask .< Inf)))
-    
-            tup       = shift!(av_toys)
-            tid       = tup[1]            
-            arrival   = tup[2] 
-            duration  = tup[3]
-            hrs_ratio = tup[4]
+        done_mask = map(x -> x < (day_start_minute + hrs._day_end) ? 1.0 : Inf, next_minutes)
+        for tid in sort([min_av_toy:max_av_toy], by= x -> myToys[3,x], rev=true)           	
+
+            if (toy_done[tid])
+                continue
+            end
+            
+            if (!any(done_mask .< Inf))
+                break
+            end
+                               
+            arrival   = myToys[2,tid] 
+            duration  = myToys[3,tid]
+            hrs_ratio = optRatios[tid]
 
             # Calculate some metrics and select Elf with minimum
             # score.
-            start_times = max(arrival, next_minutes)
+            start_times = max(arrival, next_minutes, day_start_minute)
             work_times  = int(ceil(duration ./ ratings))
             wait_times  = max(0, arrival .- next_minutes)
             rem_times   = max(0,(start_times .+ work_times ) - hrs._day_end)
@@ -109,17 +122,21 @@ function vectorSolve(myToys, num_elves)
             this_start_minute = start_times[idx] 
             this_sanctioned, this_unsanctioned =
                 get_sanctioned_breakdown(this_start_minute,  this_duration)
- 
-            if ((this_unsanctioned/ this_sanctioned) > hrs_ratio)
+
+            this_ratio = this_unsanctioned/ this_sanctioned
+            if ((this_unsanctioned > 10) && (this_ratio > hrs_ratio))
                 # This assignment doesn't achieve the optimal ratio
-                # of sanctioned and unsanctioned hours. Rschedule
+                # of sanctioned and unsanctioned hours. Reschedule
+                #println("$(tid) $(this_ratio) $(hrs_ratio) $(this_start_minute) $(this_duration) $(this_sanctioned) $(this_unsanctioned)")
                 continue
             end
 
             # Looks good, do the assignment
-            push!(assignments[idx],
-                  (tid, idx, this_start_minute, this_duration))
-
+            assign_count[idx] += 1
+            assignments[:,assign_count[idx],idx] = [tid; idx; this_start_minute; this_duration]
+            toy_done[tid] = true
+            min_av_toy    = min(min_av_toy, tid)
+            
             assigned_count += 1
             if ((assigned_count % 10000) == 0)
                 @printf("Assigned toys: %d\n", assigned_count)
@@ -147,30 +164,34 @@ function vectorSolve(myToys, num_elves)
                                               this_unsanctioned)
         end
 
-
         # Record and reset assignments
         for i in 1:num_elves
-            if (length(assignments[i]) > 0)
-                append!(jobs, assignments[i])
-                assignments[i] = Tuple[]
+            for j in 1:assign_count[i]
+                toy_id          = assignments[1,j,i]
+                elf_id          = assignments[2,j,i] 
+                work_start_time = assignments[3,j,i] 
+                work_duration   = assignments[4,j,i]
+    
+                tt = hrs._reference_start_time + Dates.Minute(work_start_time)
+                time_string = join(map(string,[Dates.year(tt) Dates.month(tt) Dates.day(tt) Dates.hour(tt) Dates.minute(tt)]), " ")
+                println(wcsv,
+                        toy_id,",",
+                        elf_id,",",
+                        time_string,",",
+                        work_duration)
+                
             end
         end
-                
-        # Carry over any left over toys
-       append!(av_toys, leftover_toys)
-                                    
-    end
 
-    for j in jobs
-        solution[1,soln_idx] = j[1]
-        solution[2,soln_idx] = j[2]
-        solution[3,soln_idx] = j[3]
-        solution[4,soln_idx] = j[4]
-        soln_idx += 1           
-    end
+        ## Clear out the assignments matrix
+        fill!(assignments,0)
+        fill!(assign_count, 0)
 
+    end
+    close(wcsv)
+        
     avg_prod = mean(ratings)
-    return solution, num_elves, last_minute, avg_prod    
+    return last_minute, avg_prod    
 end
 
 end # end module
